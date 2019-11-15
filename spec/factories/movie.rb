8fc721b70cc4c514818489f3919d7694ba7df44c
Milos Vukadinovic <milos.vukadinovic11@gmail.com>
2019-11-15 14:11:59 +0100

    Fixing faker

--- spec/factories/movie.rb
+++ spec/factories/movie.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :movie do
     title { Faker::Lorem.word }
     description { Faker::Lorem.sentence(3, true) }
-    released_at { Faker::Date.between(40.years.ago, Time.zone.today) }
+    released_at { Faker::Date.between(from: 2.days.ago, to: Date.today) }
     genre
   end
 end
